4798cbd3d117c81262ad2fbb8614cfd8eec1c6fe
Paul ALNET <paul@shimaore.net>
2024-02-09 10:42:52 +0100

    test(swagger): plugin standalone

---
+++ test/plugins/swagger.test.ts
@@ -0,0 +1,12 @@
+import { test } from 'node:test'
+import * as assert from 'node:assert'
+import Fastify from 'fastify'
+import SwaggerPlugin from '../../src/plugins/swagger.js'
+
+test('SwaggerPlugin works standalone', async () => {
+  const fastify = Fastify()
+  void fastify.register(SwaggerPlugin)
+  await fastify.ready()
+
+  assert.ok(fastify.swagger({ yaml: true }).includes('Tawkie Matrix signup queue'))
+})

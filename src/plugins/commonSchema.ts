3ffafdca3c0e3f26c8eaab7fff082083e1108e51
Paul ALNET <paul@shimaore.net>
2024-02-09 13:46:29 +0100

    chore: add common uuid schema

---
+++ src/plugins/commonSchema.ts
@@ -0,0 +1,15 @@
+import fastifyPlugin from 'fastify-plugin';
+import type { FastifyInstance } from 'fastify';
+
+async function commonSchemaPlugin(fastify: FastifyInstance) {
+  fastify.addSchema({
+    $id: 'https://tawkie.fr/common/uuid',
+    title: 'UUIDv4',
+    description: 'A UUID version 4 string',
+    type: 'string',
+    pattern: '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$',
+    example: 'df27bea8-8596-45a8-ab28-17a7332fd03a',
+  })
+}
+
+export default fastifyPlugin(commonSchemaPlugin);

3ab12d8dfbeab98cafdb2c37915e0efbbeae9e03
Paul ALNET <paul@shimaore.net>
2024-02-12 11:10:52 +0100

    fix(test): swagger stuck in loop

--- test/plugins/swagger.test.ts
+++ test/plugins/swagger.test.ts
@@ -6,6 +6,25 @@ import SwaggerPlugin from '../../src/plugins/swagger.js'
 test('SwaggerPlugin works standalone', async () => {
   const fastify = Fastify()
   void fastify.register(SwaggerPlugin)
+
+  const schema = {
+    querystring: {
+      type: 'object',
+      properties: {
+        search: { type: 'string' }
+      }
+    },
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          greeting: { type: 'string' }
+        }
+      }
+    }
+  }
+  fastify.get('/demoEndpoint', { schema }, () => 'demoResponse')
+
   await fastify.ready()
 
   assert.ok(fastify.swagger({ yaml: true }).includes('Tawkie Matrix signup queue'))

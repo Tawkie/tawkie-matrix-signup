2bceee56622f6c0b2be2fa20f59ac6e1d87acec3
Paul ALNET <paul@shimaore.net>
2024-02-14 15:06:31 +0100

    wip(updateUsername): endpoint start

---
+++ src/routes/matrix-queue/updateUsername.ts
@@ -0,0 +1,56 @@
+import { FastifyPluginAsync } from "fastify"
+import { FastifyInstance } from "fastify"
+
+type RequestBody = {
+  userId: string
+  username: string
+}
+
+export const updateUsernameSchema = {
+  body: {
+    type: 'object',
+    properties: {
+      userId: { '$ref': 'https://tawkie.fr/common/uuid' },
+      username: { '$ref': 'https://tawkie.fr/common/matrixUsername' },
+    }
+  },
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        userId: { '$ref': 'https://tawkie.fr/common/uuid' },
+      }
+    },
+    400: { $ref: 'https://tawkie.fr/common/HttpError' },
+    500: { $ref: 'https://tawkie.fr/common/HttpError' },
+  }
+}
+
+const example: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.put<{ Body: RequestBody }>('/updateUsername', { schema: updateUsernameSchema }, async function(request) {
+    const userId = request.body.userId
+    // const username = request.body.username
+    const { rows } = await ensureInQueue(fastify, userId)
+
+    // TODO
+
+    // There should be exactly one row.
+    // See /src/plugins/postgres.ts for the table spec
+
+    return {
+      userId,
+      queuePosition: rows[0].queue_position
+    }
+  })
+}
+
+async function ensureInQueue(fastify: FastifyInstance, userId: string) {
+  // query should insert the user in the queue if not already in it
+  // and return the position in the queue to avoid a race condition
+  const query = `SELECT insert_user_in_queue_if_not_exists($1) AS queue_position;`
+  // See /src/plugins/postgres.ts for the function spec
+
+  return fastify.pg.query<{ position: number }>(query, [userId])
+}
+
+export default example;

3c2b76ba99e6048f94b90559b3a3b385dce951fb
Paul ALNET <paul@shimaore.net>
2024-02-14 14:48:38 +0100

    feat(joinQueue): sql ensureUserInQueue function

--- src/plugins/postgres.ts
+++ src/plugins/postgres.ts
@@ -20,7 +20,34 @@ CREATE TABLE IF NOT EXISTS public.user_queue (
 CREATE INDEX IF NOT EXISTS idx_user_queue_user_uuid ON public.user_queue(user_uuid);
 `
   await fastify.pg.query(query);
+  await ensureUserQueueFunctionExists(fastify);
 }
+
+async function ensureUserQueueFunctionExists(fastify: FastifyInstance) {
+  const query = `
+CREATE OR REPLACE FUNCTION insert_user_in_queue_if_not_exists(user_uuid_arg UUID)
+RETURNS INTEGER AS $$
+DECLARE
+    v_queue_position INTEGER;
+BEGIN
+    -- Check if the user_uuid already exists
+    SELECT queue_position INTO v_queue_position FROM user_queue WHERE user_uuid = user_uuid_arg;
+
+    -- If not found, then insert
+    IF v_queue_position IS NULL THEN
+        INSERT INTO user_queue (user_uuid) VALUES (user_uuid_arg)
+        RETURNING queue_position INTO v_queue_position;
+    END IF;
+
+    -- Return the queue_position (either existing or new)
+    RETURN v_queue_position;
+END;
+$$ LANGUAGE plpgsql;
+`
+  // can be called with `SELECT insert_user_in_queue_if_not_exists('your-user-uuid-here');`
+  await fastify.pg.query(query);
+}
+
 
 
 export default fastifyPlugin(postgresPlugin);

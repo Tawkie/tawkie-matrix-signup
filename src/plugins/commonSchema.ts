dc162840648f98d3d6f00cfef93dd43207f58ca1
Paul ALNET <paul@shimaore.net>
2024-02-14 15:06:07 +0100

    feat(updateUsername): add matrix username schema

--- src/plugins/commonSchema.ts
+++ src/plugins/commonSchema.ts
@@ -10,6 +10,14 @@ async function commonSchemaPlugin(fastify: FastifyInstance) {
     pattern: '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$',
     examples: ['df27bea8-8596-45a8-ab28-17a7332fd03a'],
   })
+  fastify.addSchema({
+    $id: 'https://tawkie.fr/common/matrixUsername',
+    title: 'Matrix username',
+    description: 'A Matrix username',
+    type: 'string',
+    pattern: '^[a-z0-9]{3,16}$',
+    examples: ['bobby'],
+  })
 }
 
 export default fastifyPlugin(commonSchemaPlugin);

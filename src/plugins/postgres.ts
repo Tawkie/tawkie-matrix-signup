f8a9cfe7a330027c799914aa8af75af21e3d55eb
Paul ALNET <paul@shimaore.net>
2024-02-15 12:43:55 +0100

    chore(postgres): lint

--- src/plugins/postgres.ts
+++ src/plugins/postgres.ts
@@ -16,29 +16,29 @@ export const UserQueueStateStrings = {
 }
 
 async function postgresPlugin(fastify: FastifyInstance) {
-  const uri = process.env.TAWKIE_SIGNUP_POSTGRES_URI ?? 'postgres://postgres:mysecretpassword@localhost/postgres';
-  fastify.register(import('@fastify/postgres'), {
-    connectionString: uri,
-  });
-  fastify.register(ensureTableExists);
+    const uri = process.env.TAWKIE_SIGNUP_POSTGRES_URI ?? 'postgres://postgres:mysecretpassword@localhost/postgres';
+    fastify.register(import('@fastify/postgres'), {
+        connectionString: uri,
+    });
+    fastify.register(ensureTableExists);
 }
 
 async function ensureTableExists(fastify: FastifyInstance) {
-  const query = `
+    const query = `
 CREATE TABLE IF NOT EXISTS public.user_queue (
     queue_position SERIAL PRIMARY KEY,
     user_uuid UUID UNIQUE NOT NULL,
     username VARCHAR(20),
-    state INT NOT NULL DEFAULT ${UserQueueState.IN_QUEUE},
+    state INT NOT NULL DEFAULT ${UserQueueState.IN_QUEUE}
 );
 CREATE INDEX IF NOT EXISTS idx_user_queue_user_uuid ON public.user_queue(user_uuid);
 `
-  await fastify.pg.query(query);
-  await ensureUserQueueFunctionExists(fastify);
+    await fastify.pg.query(query);
+    await ensureUserQueueFunctionExists(fastify);
 }
 
 async function ensureUserQueueFunctionExists(fastify: FastifyInstance) {
-  const query = `
+    const query = `
 CREATE OR REPLACE FUNCTION insert_user_in_queue_if_not_exists(user_uuid_arg UUID)
 RETURNS INTEGER AS $$
 DECLARE
@@ -58,10 +58,8 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 `
-  // can be called with `SELECT insert_user_in_queue_if_not_exists('your-user-uuid-here');`
-  await fastify.pg.query(query);
+    // can be called with `SELECT insert_user_in_queue_if_not_exists('your-user-uuid-here');`
+    await fastify.pg.query(query);
 }
-
-
 
 export default fastifyPlugin(postgresPlugin);

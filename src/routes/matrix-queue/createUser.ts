b057a8853e25746ac6e6bd0be11a3f0860570af9
Paul ALNET <paul@shimaore.net>
2024-02-15 16:50:14 +0100

    wip(createUser): base

---
+++ src/routes/matrix-queue/createUser.ts
@@ -0,0 +1,53 @@
+import { FastifyPluginAsync } from "fastify"
+import { FastifyInstance } from "fastify"
+import { getUserFromQueue } from "./queueStatus.js"
+import { UserQueueState } from "../../plugins/postgres.js"
+
+type RequestBody = {
+  userId: string
+}
+
+export const updateUsernameSchema = {
+  body: {
+    type: 'object',
+    required: ['userId'],
+    properties: {
+      userId: { '$ref': 'https://tawkie.fr/common/uuid' },
+    }
+  },
+  response: {
+    200: {
+      type: 'object',
+      properties: {
+        userId: { '$ref': 'https://tawkie.fr/common/uuid' },
+        username: { '$ref': 'https://tawkie.fr/common/matrixUsername' },
+        userState: { '$ref': 'https://tawkie.fr/common/userQueueState' },
+        queuePosition: { type: 'integer' },
+      }
+    },
+    400: { $ref: 'https://tawkie.fr/common/HttpError' },
+    500: { $ref: 'https://tawkie.fr/common/HttpError' },
+  }
+}
+
+const example: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.put<{ Body: RequestBody }>('/createUser', { schema: updateUsernameSchema }, async function(request) {
+    const userId = request.body.userId
+
+    const user = await getUserFromQueue(fastify, userId)
+
+    // TODO actually create the user
+    await markUserAsCreated(fastify, userId)
+    // TODO How to deal with the state if the Matrix user is succesfully created but the markUserAsCreated fails?
+    // TODO write tests
+    return user
+  })
+}
+
+async function markUserAsCreated(fastify: FastifyInstance, userId: string) {
+  const query = `UPDATE user_queue SET user_state = ${UserQueueState.CREATED} WHERE user_uuid = $1;`
+  await fastify.pg.query(query, [userId])
+}
+
+
+export default example;
